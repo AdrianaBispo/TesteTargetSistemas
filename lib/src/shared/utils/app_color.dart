b041bf10a6167b3190e95c0e0b04cc837e6a7738
dev <adriana.bispo283@gmail.com>
2023-11-12 17:42:54 -0300

    feat: criada a classe app color

---
+++ lib/src/shared/utils/app_color.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static AppColor? _instance;
+  AppColor._();
+
+  static AppColor get instance {
+    _instance ??= AppColor._();
+    return _instance!;
+  }
+
+  Color get primary => Colors.white;
+  Color get secondary => const Color(0xff44BD6E);
+  Color get black => Color(0xFF212837);
+  Color get gradien1 => Color(0xFF1C5566);
+  Color get gradien2 => Color(0xFF26757A);
+  Color get gradien3 => Color(0xFF29918A);
+  
+}

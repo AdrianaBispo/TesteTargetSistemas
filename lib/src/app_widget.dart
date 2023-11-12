ad4c8c604d53488cdccc999cf417e0171240ccb1
dev <adriana.bispo283@gmail.com>
2023-11-12 17:45:11 -0300

    adcionado provider e app theme

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+//utils
+import 'shared/utils/app_theme.dart';
+//controller
+import 'features/login/controller/login_controller.dart';
+//view
+import 'features/login/view/login_view.dart';
 
 class TargetSistemas extends StatelessWidget {
   const TargetSistemas({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider<LoginController>(create: (_) => LoginController()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Teste Target Sistemas',
+        theme: AppTheme.theme,
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Login(),
+        },
       ),
     );
   }
 }
-

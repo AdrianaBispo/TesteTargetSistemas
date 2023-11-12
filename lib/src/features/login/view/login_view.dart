5edc2136155cd59ca318e0ebd209f95cead4808f
dev <adriana.bispo283@gmail.com>
2023-11-12 17:44:28 -0300

    chore: login renomeada para login view

---
+++ lib/src/features/login/view/login_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+//componentes
+import '../components/formulario_login.dart';
+import '../../components/custom_background.dart';
+import '../components/botao_privacidade.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: CustomBackground(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                //color: Colors.blue.shade50,
+                width: MediaQuery.of(context).size.width * .8,
+                height: MediaQuery.of(context).size.height * .7,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FormularioLogin(),
+                    BotaoPrivacidade(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
